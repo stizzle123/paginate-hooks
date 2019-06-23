03026efa1e12df79d581289da3c61e58f8158b19
stizzle123 <arifayanidowu2@yahoo.com>
2019-06-23 20:40:02 +0100

    Simple pagination boilerplate using react hooks

---
+++ src/components/Posts.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Posts = ({ posts, loading }) => {
+    if(loading) {
+        return <h4>Loading...</h4>
+    }
+    return (
+        <ul className="list-group mb-4">
+            {posts.map(post => (
+                <li key={post.id} className="list-group-item">
+                    {post.title}
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default Posts
